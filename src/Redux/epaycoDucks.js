527407495fe14212381e6e14e46a98a37add7f6e
Andres Silva Alvarez <jandresi@hotmail.com>
2022-03-12 12:05:47 -0500

    Redux getToken funcionando 2

---
+++ src/Redux/epaycoDucks.js
@@ -0,0 +1,37 @@
+// Constantes
+const tokenInicial = {
+    token: ''
+}
+
+const GET_TOKEN = 'GET_TOKEN'
+
+// Reducers
+export default function tokenReducer(state = tokenInicial, action) {
+    switch(action.type) {
+        case GET_TOKEN:
+            return {token: action.payload}
+        default:
+            return state
+    }
+}
+
+// Acciones
+export const getTokenAccion = () => async(dispatch, getState) => {
+    try {
+        const api = await fetch("https://apify.epayco.co/login/mail", {
+            method: "post",
+            headers: {
+                Authorization: "Basic " + btoa("pruebafront@payco.co:pruebafront$2020"),
+            },
+            mode: "cors",
+            cache: "default",
+        });
+        const token = await api.json().then(res => res.token);
+        dispatch({
+            type: GET_TOKEN,
+            payload: token
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
